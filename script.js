1d2357dc05ade4f4dc24c27098feb2db83c982e7
Simeon Oyetunde <simeon.aoyetunde@gmail.com>
2023-03-31 09:53:28 +0100

    Second commit :Mostly working js

--- script.js
+++ script.js
@@ -20,5 +20,29 @@ tabs.forEach(tab => {
             gameOver = true;
             return;
         }
-    })
-})
+
+        player1 = player1==='X'?'O':'X';
+    });
+});
+
+function checkWin() {
+    const winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+        [0, 4, 8], [2, 4, 6] // diagonals
+    
+    ];
+
+    return winConditions.some(condition=> {
+        return condition.every(index=>{
+            return tabs[index].textContent===player1;
+        });
+    });
+}
+
+
+function checkTie() {
+    return Array.from(tabs).every(tab=> {
+        return tab.textContent!=='';
+    });
+}
